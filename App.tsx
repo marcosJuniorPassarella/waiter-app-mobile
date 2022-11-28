5152b77b9bfbca8173457382d5ee8d7d0113aefb
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-11-28 20:59:15 -0300

    Added customize fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  useFonts({
+    "GeneralSans-400": require("./src/assets/fonts/GeneralSans-Regular.otf"),
+    "GeneralSans-500": require("./src/assets/fonts/GeneralSans-Semibold.otf"),
+    "GeneralSans-600": require("./src/assets/fonts/GeneralSans-Bold.otf"),
+  });
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -14,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#292929',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#292929",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
