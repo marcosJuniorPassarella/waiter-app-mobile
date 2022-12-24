4cfd580d4dab14d8e04ba399df4bae31fb938da3
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-12-24 15:55:42 -0300

    Created main component

---
+++ src/Main/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text } from "../components/Text";
+import { Container } from "./styles";
+
+export function Main() {
+  return (
+    <Container>
+      <Text> Main renderizou</Text>
+    </Container>
+  );
+}
